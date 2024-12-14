3d8c906d09f2bb9bb53b07a531f6bc30b56632aa
Kannan <kannan.iosdev8@gmail.com>
2024-12-14 22:53:07 +0530

    Initial Commit

---
+++ TestXcodeCloud/TestXcodeCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestXcodeCloudApp.swift
+//  TestXcodeCloud
+//
+//  Created by Kannan R S on 14/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TestXcodeCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
